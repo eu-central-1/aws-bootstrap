d96bd774e17f3151dc407a3ff2c74f54317222f9
Stefan Bauer <stefan.bauer@unbelievable-machine.com>
2020-04-10 19:31:02 +0200

    UPDATE stack name to new convention and improved understanding

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from aws_cdk import core
 from cdk_stack.aws_multiaccount_setup.init_stack import InitStack
 
 app = core.App()
-InitStack(app, "aws_multiaccount_setup")
+InitStack(app, "aws-bootstrap-multiaccount")
 
 app.synth()
